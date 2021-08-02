387678891e9d7aeb084fa9984e37b533bf83ef40
iszon <wojtekst64@gmail.com>
2021-08-02 15:07:45 +0200

    lavender: Use AOSP-11 hals

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -1,2 +1,10 @@
+# Nuke
+rm -rf hardware/qcom-caf/msm8998/display
+rm -rf hardware/qcom-caf/msm8998/media
+rm -rf hardware/qcom-caf/msm8998/audio
+
 # Clone
 git clone -b 11 https://github.com/iszon/vendor_xiaomi_Gcam.git vendor/xiaomi/Gcam
+git clone https://github.com/iszon/hardware_qcom-caf_display_msm8998 hardware/qcom-caf/msm8998/display
+git clone https://github.com/iszon/hardware_qcom-caf_media_msm8998 hardware/qcom-caf/msm8998/media
+git clone https://github.com/iszon/hardware_qcom-caf_audio_msm8998 hardware/qcom-caf/msm8998/audio
