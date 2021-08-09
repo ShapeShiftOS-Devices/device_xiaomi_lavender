78238796c7ff3a0c5338e32378d3d3d140ac03cc
Iszon <wojtekst64@gmail.com>
2021-08-09 19:10:25 +0200

    lavender: Use msm8998-r HALs (1/2)

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -5,6 +5,6 @@ rm -rf hardware/qcom-caf/msm8998/audio
 
 # Clone
 git clone -b 11 https://github.com/iszon/vendor_xiaomi_Gcam.git vendor/xiaomi/Gcam
-git clone https://github.com/iszon/hardware_qcom-caf_display_msm8998 hardware/qcom-caf/msm8998/display
-git clone https://github.com/iszon/hardware_qcom-caf_media_msm8998 hardware/qcom-caf/msm8998/media
-git clone https://github.com/iszon/hardware_qcom-caf_audio_msm8998 hardware/qcom-caf/msm8998/audio
+git clone https://github.com/iszon/hardware_qcom-caf_display_msm8998 hardware/qcom-caf/msm8998-r/display
+git clone https://github.com/iszon/hardware_qcom-caf_media_msm8998 hardware/qcom-caf/msm8998-r/media
+git clone https://github.com/iszon/hardware_qcom-caf_audio_msm8998 hardware/qcom-caf/msm8998-r/audio
